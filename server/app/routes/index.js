2f595fa363361389b8761e6bc6228ad97fe1dd97
Kaito Hara <kaito.hara@me.com>
2015-07-27 16:26:41 -0400

    genre routes

--- server/app/routes/index.js
+++ server/app/routes/index.js
@@ -4,6 +4,8 @@ module.exports = router;
 
 router.use('/members', require('./members'));
 
+router.use('/genre', require('./genre'));
+
 // Make sure this is after all of
 // the registered routes!
 router.use(function (req, res) {
