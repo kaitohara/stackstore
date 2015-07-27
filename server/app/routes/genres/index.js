e0850d94a18f099291353cb97b021d1472ba04df
Kaito Hara <kaito.hara@me.com>
2015-07-27 16:21:34 -0400

    genre route

---
+++ server/app/routes/genres/index.js
@@ -0,0 +1,35 @@
+'use strict';
+var router = require('express').Router();
+module.exports = router;
+
+var mongoose = require('mongoose');
+
+var genre = mongoose.model('Genre')
+
+router.use('/genre', require('./genre'));
+
+router.get('/', function (req, res){
+	genre.find({}).exec()
+		.then(function(data){
+			res.json(data);
+		})
+		.then(null, function(err){
+			res.json(err)
+		})
+})
+
+router.get('/:genre', function (req, res){
+	genre.find({name:req.params.genre}).exec()
+		.then(function(data){
+			res.json(data);
+		})
+		.then(null, function(err){
+			res.json(err)
+		})
+})
+
+// Make sure this is after all of
+// the registered routes!
+router.use(function (req, res) {
+    res.status(404).end();
+});
