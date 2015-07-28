ae9b612e2eabd6c2861188f1497a5d7553b188ab
Jack Mulrow <jackmulrow@Jacks-MacBook-Pro.local>
2015-07-27 21:54:30 -0400

    split order.items into songs and albums arrays

--- server/db/models/order.js
+++ server/db/models/order.js
@@ -2,14 +2,29 @@
 var mongoose = require('mongoose');
 
 var schema = new mongoose.Schema({
-	items: [{
-        	// stores cached price and reference to song
-            id: String,
-            // true if album, false if song
-            itemType: {type: String, enum: ['album', 'song']},
-            price: Number,
-            quantity: Number
-    }],
+	// items: [{
+ //        	// stores cached price and reference to song
+ //            id: String,
+ //            // item: {type: mongoose.Schema.Types.ObjectId, ref:}
+ //            // true if album, false if song
+ //            itemType: {type: String, enum: ['album', 'song']},
+ //            price: Number,
+ //            quantity: Number
+ //    }],
+ 	songs: [{
+ 		song: {type: mongoose.Schema.Types.ObjectId, ref: 'Song'},
+ 		price: {
+ 			type: Number
+ 		},
+ 		quantity: Number
+ 	}],
+ 	albums: [{
+ 		album: {type: mongoose.Schema.Types.ObjectId, ref: 'Album'},
+ 		price: {
+ 			type: Number
+ 		},
+ 		quantity: Number
+ 	}],
 	date: {
 		created: {type: Date, default: Date.now},
 		// finished = cancelled OR completed
