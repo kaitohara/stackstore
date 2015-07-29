97a6dae2b886368c527429056ae21ca655736872
joanaz <rz258@cornell.edu>
2015-07-29 02:23:08 -0400

    c

--- browser/js/profile/profile.js
+++ browser/js/profile/profile.js
@@ -6,15 +6,28 @@ app.config(function($stateProvider) {
     controller: 'ProfileController',
     templateUrl: 'js/profile/profile.html',
     resolve: {
-      user: function(AuthService) {
-        return AuthService.getLoggedInUser();
+      user: function(AuthService, ProfileFactory) {
+        return AuthService.getLoggedInUser()
+          .then(function(currentUser) {
+            return ProfileFactory.populateAll(currentUser)
+          })
       }
     }
   });
 });
 
 app.controller('ProfileController', function($scope, user) {
-
   $scope.user = user;
-
 });
+
+app.factory('ProfileFactory', function($http) {
+  return {
+    populateAll: function(user) {
+      console.log(user)
+      return $http.get("api/users/" + user._id + '/profile')
+        .then(function(res) {
+          return res.data
+        })
+    }
+  }
+})
--- server/app/routes/users/index.js
+++ server/app/routes/users/index.js
@@ -33,7 +33,72 @@ router.param('id', function(req, res, next, id) {
 })
 
 router.get('/:id', function(req, res) {
-    res.send(req.user);
+    res.json(req.user)
+});
+
+router.get('/:id/profile', function(req, res) {
+    console.log('hi')
+    var options1 = {
+        path: 'pastOrderList',
+        model: 'Order'
+    };
+    User.populate(req.user, options1)
+        .then(function(popUser) {
+            var options2 = {
+                path: 'pastOrderList.albums.album',
+                model: 'Album'
+            };
+            User.populate(popUser, options2)
+                .then(function(popedUser) {
+                    var options3 = {
+                        path: 'pastOrderList.songs.song',
+                        model: 'Song'
+                    };
+                    User.populate(popedUser, options3)
+                        .then(function(populatedUser) {
+                            var options4 = {
+                                path: 'pastOrderList.albums.album.artist pastOrderList.songs.song.artist',
+                                model: 'Artist'
+                            };
+                            User.populate(populatedUser, options4)
+                                .then(function(finalUser) {
+                                    res.json(finalUser)
+                                })
+                        })
+                })
+        })
+});
+
+router.get('/:id/cart', function(req, res) {
+    var options1 = {
+        path: ' cart',
+        model: 'Order'
+    };
+    User.populate(req.user, options1)
+        .then(function(popUser) {
+            var options2 = {
+                path: ' cart.albums.album',
+                model: 'Album'
+            };
+            User.populate(popUser, options2)
+                .then(function(popedUser) {
+                    var options3 = {
+                        path: ' cart.songs.song',
+                        model: 'Song'
+                    };
+                    User.populate(popedUser, options3)
+                        .then(function(populatedUser) {
+                            var options4 = {
+                                path: 'cart.albums.album.artist cart.songs.song.artist',
+                                model: 'Artist'
+                            };
+                            User.populate(populatedUser, options4)
+                                .then(function(finalUser) {
+                                    res.json(finalUser)
+                                })
+                        })
+                })
+        })
 });
 
 router.put('/:id', function(req, res) {
