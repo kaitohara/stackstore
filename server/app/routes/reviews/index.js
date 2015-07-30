13cc454ef0f958188ca0005253b40210f8a152b3
bryan chu <shino.sk@gmail.com>
2015-07-30 09:38:49 -0400

    updated review router to have subroutes for single review and multiple reviews

--- server/app/routes/reviews/index.js
+++ server/app/routes/reviews/index.js
@@ -1,53 +1,8 @@
 'use strict';
 
 var router = require('express').Router();
-var mongoose = require('mongoose');
-var review = mongoose.model('Review');
 
-
-router.param('reviewId', function(req, res, next, reviewId){
-    review.findById(reviewId)
-        .then(function(reviewItem){
-            req.reviewItem = reviewItem;
-            next();
-        })
-        .then(null, function(err){
-            next(err);
-        });
-});
-
-router.get('/', function(req, res, next){
-    review.find().exec()
-        .then(function(reviewItems){
-            res.json(reviewItems);
-        })
-        .then(null, function(err){
-            next(err);
-        })
-});
-
-router.get('/:reviewId', function(req, res){
-    res.json(req.reviewItem);
-});
-
-router.put('/:reviewId', function(req, res, next){
-    req.order.save()
-        .then(function(reviewItem){
-            res.json(reviewItem);
-        })
-        .then(null,next);
-});
-
-router.post('/', function(req, res, next){
-    review
-        .create(req.body)
-        .then(function(reviewItem){
-            if(!reviewItem) throw new Error();
-            else return reviewItem;
-        })
-        .then(null, function(err){
-            next(err);
-        });
-});
+router.use('/review', require('./review.router.js'));
+router.use('/reviews', require('./reviews.router.js'));
 
 module.exports = router;
