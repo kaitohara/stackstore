7b1d403ce666fd4dd6ed7f010380c989493bc472
Kaito Hara <kaito.hara@me.com>
2015-07-30 17:29:33 -0400

    search CSS/html

--- browser/js/search/product/songs.factory.js
+++ browser/js/search/product/songs.factory.js
@@ -3,7 +3,7 @@
 app.factory('songs', function ($http){
 	return {
 		getSongs: function(){
-			return $http.get('/api/songs').then(function(response){
+			return $http.get('/api/songs/populated').then(function(response){
 				return response.data;
 			})
 		}
