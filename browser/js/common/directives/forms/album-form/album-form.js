eb777e1b1d615dad5dec7228981477c2632fe5f9
Jack Mulrow <jackmulrow@Jacks-MacBook-Pro.local>
2015-08-03 00:14:42 -0400

    added categories field to album form

--- browser/js/common/directives/forms/album-form/album-form.js
+++ browser/js/common/directives/forms/album-form/album-form.js
@@ -1,13 +1,16 @@
-app.directive('albumForm', function(){
+app.directive('albumForm', function(FormFactory){
 	// Runs during compile
 	return {
 		restrict: 'E',
 		templateUrl: 'js/common/directives/forms/album-form/album-form.html',
 		scope: {
-			submit: '&'
+			submit: '&',
+			admin: '='
 		},
-		link: function() {
+		link: function(scope, elem, attrs) {
 			console.log('rna album form directive');
+			FormFactory.getGenres()
+				.then(genres => scope.genres = genres);
 		}
 	};
 });
