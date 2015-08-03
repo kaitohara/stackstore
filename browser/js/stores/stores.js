1f01895582bf9395e3622aa9950da00351fca476
Jack Mulrow <jackmulrow@Jacks-MacBook-Pro.local>
2015-08-03 09:57:05 -0400

    store list is now a directive

---
+++ browser/js/stores/stores.js
@@ -0,0 +1,10 @@
+app.directive('storeList', function() {
+	return {
+		restrict: 'E',
+		// scope: {},
+		templateUrl: 'js/stores/stores.html',
+		link: function(scope, elem, attrs) {
+			
+		}
+	};
+});
