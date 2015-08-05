ffbd5d45600cc5a247c10dfc385fcc261c7c5661
Kaito Hara <kaito.hara@me.com>
2015-08-05 01:10:54 -0400

    Checkout sends confirmation email

--- browser/js/user/checkout/checkout.controller.js
+++ browser/js/user/checkout/checkout.controller.js
@@ -1,29 +1,27 @@
-app.controller('CheckoutCtrl', function ($scope, cart, $state){
+app.controller('CheckoutCtrl', function ($scope, cart, $state, checkoutFactory){
 	$scope.cart = cart;
 	// $scope.getTotal = function(){
 	// 	var total = 0;
 	// 	$scope.cart.song
 	// }
 	$scope.submitted = false;
-	$scope.submit = function(){
-
-	}
+	$scope.submitSuccess = false;
 	$scope.number = 10;
+	$scope.email
 	$scope.getNumber = function(num) {
-    	return new Array(num);   
+		return new Array(num);   
 	}
 	$scope.submitForm = function(isValid) {
 
-	$scope.custEmail
-    // check to make sure the form is completely valid
-   $scope.submitted = true;
-    if (isValid) {
-      $scope.submitSuccess = true;
-      $scope.goodToGo = true;
-      	
-		setTimeout(function(){
-			$state.go('complete')
-		}, 5000)
-    }
-  };
+		$scope.submitted = true;
+		if (isValid) {
+			$scope.submitSuccess = true;
+			$scope.goodToGo = true;
+			setTimeout(function(){
+				checkoutFactory.submitOrder($scope.email, $scope.cart).then(function(data){
+					$state.go('complete')
+				})
+			}, 5000)
+		}
+	};
 })
